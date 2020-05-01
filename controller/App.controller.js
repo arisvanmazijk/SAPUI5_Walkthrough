27b7f6475540ce15e4a918b8018342bf4273cf62
arisvanmazijk <arisvanmazijk@hotmail.com>
2020-05-01 08:56:16 +0000

    Step 10

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,11 +1,14 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-	], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+	], function (Controller, MessageToast) {
 		"use strict";
 		return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
 			onShowHello : function () {
-				//Show native JavaScript alert
-				alert("Hello World");
+				let oBundle = this.getView().getModel("i18n").getResourceBundle();
+				let sRecipient = this.getView().getModel().getProperty("/recipient/name");
+				let sMessage = oBundle.getText("helloMsg", [sRecipient]);
+				MessageToast.show(sMessage);
 			}
 	});
 });
