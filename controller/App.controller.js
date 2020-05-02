8e1fdc887c45e8cfd90bd02412dc991bcbfb9966
arisvanmazijk <arisvanmazijk@hotmail.com>
2020-05-02 07:03:56 +0000

    Step 15

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,14 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-	], function (Controller, MessageToast) {
+	"sap/ui/core/mvc/Controller"
+	], function (Controller) {
 		"use strict";
 		return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-			onShowHello : function () {
-				let oBundle = this.getView().getModel("i18n").getResourceBundle();
-				let sRecipient = this.getView().getModel().getProperty("/recipient/name");
-				let sMessage = oBundle.getText("helloMsg", [sRecipient]);
-				MessageToast.show(sMessage);
-			}
 	});
 });
